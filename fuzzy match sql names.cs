b34241ceaa3c00141b144a33f6b5548fc7d1c58b
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2021-10-21 12:31:58 +0100

    Update fuzzy match sql names.cs

--- fuzzy match sql names.cs
+++ fuzzy match sql names.cs
@@ -21,15 +21,14 @@ namespace FuzzyMatch
 
             Console.WriteLine("Getting Connection ...");
 
-            var datasource = @"hsc-sql-2016\BITEAM";//your server
-            var database = "TrakCareBI"; //your database name
+            var datasource = @"hsc-sql-2016\BITEAM";
+            var database = "TrakCareBI"; 
 
-
-            //connection string 
+            //Connection string 
             string connString = @"Data Source=" + datasource + ";Initial Catalog="
                         + database + ";Persist Security Info=True;Trusted_Connection=True";
 
-            //create instanace of database connection
+            //Create instanace of database connection
             SqlConnection conn = new SqlConnection(connString);
             conn.Open();
 
@@ -37,25 +36,28 @@ namespace FuzzyMatch
 SELECT *
 FROM OPENQUERY(HSSDPRD, 
 
-'SELECT TOP 15000
+'SELECT TOP 5000
          PAPMI_No as URN
        , PAPMI_Name2 as FirstName
        , PAPMI_Name as LastName
        , PAPMI_RowId->PAPER_Dob as DOB
+       , PAPMI_RowId->PAPER_Sex_DR->CTSEX_Desc as Gender
+
 
 FROM PA_PatMas
 
 WHERE PAPMI_Name2 NOT LIKE ''zz%''
 AND PAPMI_Name NOT LIKE ''zz%''
 
-
 ')";
 
+            //Create DataTable to hold SQL query data and fill
             DataTable table = new DataTable();
             table.Columns.Add("URN", typeof(int));
             table.Columns.Add("FirstName", typeof(string));
             table.Columns.Add("LastName", typeof(string));
             table.Columns.Add("DOB", typeof(string));
+            table.Columns.Add("Gender", typeof(string));
 
             SqlCommand cmd = new SqlCommand(namesSQL, conn);
 
@@ -70,11 +72,23 @@ AND PAPMI_Name NOT LIKE ''zz%''
             TimeSpan SqlTime = watch.Elapsed;
             Console.WriteLine($"SQL took {SqlTime.Minutes} minuites and {SqlTime.Seconds} seconds to return query");
             watch.Restart();
-            Console.WriteLine("Working...");
+            Console.WriteLine("Working...\n");
 
-            DataView dv = table.DefaultView;
-            dv.Sort = "FirstName";
-            DataTable sortedDT = dv.ToTable();
+            //Filter by gender and split data into 2 DataTables before adding to DataSet
+            DataView dvF = table.DefaultView;
+            dvF.RowFilter = "Gender = 'Female'";
+            DataTable femaleDT = dvF.ToTable();
+
+            DataView dvM = table.DefaultView;
+            dvF.RowFilter = "Gender = 'Male'";
+            DataTable maleDT = dvF.ToTable();
+
+            DataSet GendersDS = new DataSet();
+            GendersDS.Tables.Add(femaleDT);
+            GendersDS.Tables.Add(maleDT);
+
+            GendersDS.Tables[0].TableName = "Female";
+            GendersDS.Tables[1].TableName = "Male";
 
             string[] letters =
             {
@@ -112,81 +126,78 @@ AND PAPMI_Name NOT LIKE ''zz%''
 
             Dictionary<int, string> rowsListDict = new Dictionary<int, string>();
 
-            var n = 0;
-            foreach (DataRow row in sortedDT.Rows)
+            foreach(DataTable genderGroup in GendersDS.Tables)
+
             {
-                value = value += string.Join(" ", row["URN"].ToString(), row["FirstName"].ToString(), row["LastName"].ToString(), row["DOB"].ToString().Replace("00:00:00", ""));
-                rowsListDict.Add(n, value);
-                value = string.Empty;
-                n = n + 1;
-            }
-
-            foreach (var pair in rowsListDict)
-            {
-
-                Console.WriteLine($"{pair.Key}: {pair.Value}");
-            }
-
-
-            DataTable final = new DataTable();
-            final.Columns.Add("X", typeof(string));
-            final.Columns.Add("Y", typeof(string));
-            final.Columns.Add("Score", typeof(int));
-
-            foreach (var letter in letters)
-            {
-                for (int i = 0; i < rowsListDict.Count - 1; i++)
+                var n = 0;
+                foreach (DataRow row in genderGroup.Rows)
                 {
-                    for (int j = i + 1; j < rowsListDict.Count; j++)
+                    value = value += string.Join(" ", row["URN"].ToString(), row["FirstName"].ToString(), row["LastName"].ToString(), row["DOB"].ToString().Replace("00:00:00", ""));
+                    rowsListDict.Add(n, value);
+                    value = string.Empty;
+                    n = n + 1;
+                }
+
+                DataTable final = new DataTable();
+                final.Columns.Add("X", typeof(string));
+                final.Columns.Add("Y", typeof(string));
+                final.Columns.Add("Score", typeof(int));
+
+                foreach (var letter in letters)
+                {
+                    for (int i = 0; i < rowsListDict.Count - 1; i++)
                     {
-                        var matchResult1 = Regex.Match(rowsListDict[i], @"^([\w\-]+)");
-                        var firstWord1 = matchResult1.Value;
-                        var name1 = rowsListDict[i].Substring(firstWord1.Length + 1);
-
-                        var matchResult2 = Regex.Match(rowsListDict[i], @"^([\w\-]+)");
-                        var firstWord2 = matchResult2.Value;
-                        var name2 = rowsListDict[j].Substring(firstWord2.Length + 1);
-
-                        if (name1.StartsWith(letter.ToString()) && name2.StartsWith(letter))    //name1.StartsWith(letter.ToString()) && name2.StartsWith(letter)
+                        for (int j = i + 1; j < rowsListDict.Count; j++)
                         {
-                            var ratio = Fuzz.Ratio(name1, name2);
+                            var matchResult1 = Regex.Match(rowsListDict[i], @"^([\w\-]+)");
+                            var firstWord1 = matchResult1.Value;
+                            var name1 = rowsListDict[i].Substring(firstWord1.Length + 1);
 
-                            if (ratio < 100 && ratio > 95)
+                            var matchResult2 = Regex.Match(rowsListDict[i], @"^([\w\-]+)");
+                            var firstWord2 = matchResult2.Value;
+                            var name2 = rowsListDict[j].Substring(firstWord2.Length + 1);
+
+                            if (name1.StartsWith(letter.ToString()) && name2.StartsWith(letter))    //name1.StartsWith(letter.ToString()) && name2.StartsWith(letter)
                             {
-                                final.Rows.Add(rowsListDict[i], rowsListDict[j], ratio);
-                                Console.WriteLine($"{name1} \t-->\t{name2} \t=\t{ratio} similarity");
+                                var ratio = Fuzz.Ratio(name1, name2);
+
+                                if (ratio < 100 && ratio > 90)
+                                {
+                                    final.Rows.Add(rowsListDict[i], rowsListDict[j], ratio);
+                                    Console.WriteLine($"{name1} \t-->\t{name2} \t=\t{ratio} similarity");
+                                }
                             }
                         }
                     }
                 }
+
+                var xlsxFile = $@"M:\My Documents\Tests\FuzzyMatch\{genderGroup}_FuzzyResults.xlsx";
+
+                if (File.Exists(xlsxFile))
+                {
+                    File.Delete(xlsxFile);
+                }
+
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                FileInfo fileInfo = new FileInfo(xlsxFile);
+                using (ExcelPackage package = new ExcelPackage(fileInfo))
+                {
+
+                    ExcelWorksheet ws = package.Workbook.Worksheets.Add("Fuzzies");
+
+                    ws.Cells["A1"].LoadFromDataTable(final, true);
+                    ws.Cells.AutoFitColumns();
+                    ws.Cells.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.View.FreezePanes(2, 1);
+                    package.Save();
+                    package.Dispose();
+                }
+                rowsListDict.Clear();
             }
-
-            var xlsxFile = $@"M:\My Documents\Tests\FuzzyMatch\FuzzyResults.xlsx";
-
-            if (File.Exists(xlsxFile))
-            {
-                File.Delete(xlsxFile);
-            }
-
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            FileInfo fileInfo = new FileInfo(xlsxFile);
-            using (ExcelPackage package = new ExcelPackage(fileInfo))
-            {
-
-                ExcelWorksheet ws = package.Workbook.Worksheets.Add("Fuzzies");
-
-                ws.Cells["A1"].LoadFromDataTable(final, true);
-                ws.Cells.AutoFitColumns();
-                ws.Cells.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                ws.View.FreezePanes(2, 1);
-                package.Save();
-                package.Dispose();
-                watch.Stop();
-                TimeSpan C_SharpTime = watch.Elapsed;
-                Console.WriteLine($"SQL took {C_SharpTime.Minutes} minuites and {C_SharpTime.Seconds} seconds to process and write the data.");
-                Console.WriteLine("Finished!");
-
-            }
-        }
+            watch.Stop();
+            TimeSpan C_SharpTime = watch.Elapsed;
+            Console.WriteLine($"SQL took {C_SharpTime.Minutes} minuites and {C_SharpTime.Seconds} seconds to process and write the data.");
+            Console.WriteLine("Finished!");
+        }  
     }
 }
